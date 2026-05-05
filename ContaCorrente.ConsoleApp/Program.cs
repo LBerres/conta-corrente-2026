ea3f5dcc36d401619c418fef85e6ea193148ef02
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-05-05 19:34:44 -0300

    Adicionado Estrutura

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -16,3 +16,33 @@ o limite de saque permitido, que corresponde à soma do saldo disponível com o 
 conta.
 */
 
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("---------------------------------------------");
+    Console.WriteLine($"Conta Corrente");
+    Console.WriteLine("---------------------------------------------");
+    Console.WriteLine("1 - Saque");
+    Console.WriteLine("2 - Depósito");
+    Console.WriteLine("3 - Consulta de Saldo");
+    Console.WriteLine("S - Sair");
+    Console.WriteLine("---------------------------------------------");
+    Console.Write("> ");
+    string? opcaoMenu = Console.ReadLine();
+
+    if (opcaoMenu?.ToUpper() == "S")
+        break;
+
+    if (opcaoMenu == "1")
+    {
+
+    }
+    else if (opcaoMenu == "2")
+    {
+
+    }
+    else if (opcaoMenu == "3")
+    {
+
+    }
+}
