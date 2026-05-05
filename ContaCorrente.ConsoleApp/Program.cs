3d9e5b62c15af7f7a19ee7285d10812c87140bd6
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-05-05 19:30:11 -0300

    Adicionado Gitignore e adicionado comentário do projetp

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Especificação da Conta Corrente
+Uma conta corrente é composta por diversos atributos essenciais para sua operação. Ela possui um número
+de identificação único, um saldo disponível e um limite de débito estabelecido.
+Cada movimentação registrada na conta corresponde a uma transação financeira, que pode ser classificada
+como crédito (depósito ou entrada de dinheiro) ou débito (saque ou retirada de fundos). Cada transação
+contém um valor associado e impacta diretamente o saldo da conta.
+As operações disponíveis para uma conta corrente incluem:
+Saque: Permite a retirada de valores, respeitando o limite máximo permitido.
+Depósito: Possibilita a adição de fundos à conta.
+Consulta de saldo: Fornece informações atualizadas sobre o montante disponível.
+Transferência entre contas: Permite a movimentação de valores entre contas correntes
+distintas.
+No que se refere aos saques, a conta corrente só pode efetuar retiradas caso o valor solicitado não ultrapasse
+o limite de saque permitido, que corresponde à soma do saldo disponível com o limite adicional concedido à
+conta.
+*/
+
